b64dd47274341514126965965b9687acec7c3208
Balraj Choure <147970919+balrajjchouree@users.noreply.github.com>
2026-02-24 16:13:36 +0530

    Time: 52 ms (9.06%) | Memory: 53.8 MB (56.35%) - LeetSync

---
+++ 2775-generate-fibonacci-sequence/generate-fibonacci-sequence.js
@@ -0,0 +1,18 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let a = 0, b = 1;
+
+    while(true){
+        yield a;
+        b = a + b;
+        a = b - a;
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
