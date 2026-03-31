a1dea5173b070984aa4bdd6eebda2b457cc8b087
Balraj Choure <147970919+balrajjchouree@users.noreply.github.com>
2026-03-31 23:17:12 +0530

    Time: 5 ms (97.76%) | Memory: 44.1 MB (70.16%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,24 +1,4 @@
 class Solution {
-    private boolean isAnagram(String s1, String s2){
-        int[] count = new int[26];
-
-        for(char ch : s1.toCharArray()){
-            count[ch - 'a']++;
-        }
-
-        for(char ch : s2.toCharArray()){
-            count[ch - 'a']--;
-        }
-
-        for(int i = 0; i < 26; i++){
-            if(count[i] != 0){
-                return false;
-            }
-        }
-
-        return true;
-    }
-    
     public boolean checkInclusion(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
@@ -27,11 +7,28 @@ class Solution {
             return false;
         }
 
-        for(int i = 0; i <= m - n; i++){
-            String sub = s2.substring(i, i + n);
-            if(isAnagram(sub, s1)){
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+
+        for(int i = 0; i < n; i++){
+            count1[s1.charAt(i) - 'a']++;
+            count2[s2.charAt(i) - 'a']++;
+        }
+
+        if(Arrays.equals(count1, count2)){
+            return true;
+        }
+
+        int j = 0;
+        for(int i = n; i <  m; i++){
+            count2[s2.charAt(i) - 'a']++;
+            count2[s2.charAt(j) - 'a']--;
+
+            if(Arrays.equals(count1, count2)){
                 return true;
             }
+
+            j++;
         }
 
         return false;
