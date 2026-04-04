efefb0f38b9f7ab68a32f542159b338849f4f038
Balraj Choure <147970919+balrajjchouree@users.noreply.github.com>
2026-04-04 23:24:50 +0530

    Time: 14 ms (28.96%) | Memory: 50.2 MB (22.56%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,24 +1,36 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-       List<List<String>> ans = new ArrayList<>();
-        int n = strs.length;
+    private String getHash(String str){
+        StringBuilder hash = new StringBuilder();
+        int[] freq = new int[26];
 
-        HashMap<String, List<String>> map = new HashMap<>();
-        for(int i = 0; i < n; i++){
-            String str = strs[i];
-            char[] chArr = strs[i].toCharArray();
-            Arrays.sort(chArr);
-            String sortedStr = new String(chArr);
-
-            if(!map.containsKey(sortedStr)){
-                map.put(sortedStr, new ArrayList<>());
-            }
-
-            map.get(sortedStr).add(str);
+        for(char ch : str.toCharArray()){
+            freq[ch - 'a']++;
         }
 
-        for(List<String> group : map.values()){
-            ans.add(group);
+        for(int i = 0; i < 26; i++){
+            if(freq[i] != 0){
+                hash.append(freq[i]);
+            }
+            hash.append("$");
+        }
+
+        return hash.toString();
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> ans = new ArrayList<>();
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for(int i = 0; i < strs.length; i++){
+            String key = getHash(strs[i]);
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(strs[i]);
+        }
+
+        for(List<String> grp : map.values()){
+            ans.add(grp);
         }
 
         return ans;
